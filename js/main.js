a47dbd3bba20deba04033ada5ac222ca66445ad9
jwlee <leejeongwon87@gmail.com>
2018-09-12 18:34:42 +0900

    uncomplete

--- js/main.js
+++ js/main.js
@@ -1,85 +1,90 @@
 ;(function () {
 	
 	'use strict';
-
-	var mobileMenuOutsideClick = function() {
-
-		$(document).click(function (e) {
-	    var container = $("#gtco-offcanvas, .js-gtco-nav-toggle");
-	    if (!container.is(e.target) && container.has(e.target).length === 0) {
-	    	$('.js-gtco-nav-toggle').addClass('');
-
-	    	if ( $('body').hasClass('offcanvas') ) {
-
-    			$('body').removeClass('offcanvas');
-    			$('.js-gtco-nav-toggle').removeClass('active');
-				
-	    	}
-            
-	    }
-		});
-
-	};
-
-
-	var offcanvasMenu = function() {
-
-		$('#page').prepend('<div id="gtco-offcanvas" />');
-		//$('#page').prepend('<a href="#" class="js-gtco-nav-toggle gtco-nav-toggle "><i></i></a>');
-		var clone1 = $('.menu-1 > ul').clone();
-		$('#gtco-offcanvas').append(clone1);
-		var clone2 = $('.menu-2 > ul').clone();
-		$('#gtco-offcanvas').append(clone2);
-
-		$('#gtco-offcanvas .has-dropdown').addClass('offcanvas-has-dropdown');
-		$('#gtco-offcanvas')
-			.find('li')
-			.removeClass('has-dropdown');
-
-		// Hover dropdown menu on mobile
-		$('.offcanvas-has-dropdown').mouseenter(function(){
-			var $this = $(this);
-
-			$this
-				.addClass('active')
-				.find('ul')
-				.slideDown(500, 'easeOutExpo');				
-		}).mouseleave(function(){
-
-			var $this = $(this);
-			$this
-				.removeClass('active')
-				.find('ul')
-				.slideUp(500, 'easeOutExpo');				
-		});
-
-
-		$(window).resize(function(){
-
-			if ( $('body').hasClass('offcanvas') ) {
-
-    			$('body').removeClass('offcanvas');
-    			$('.js-gtco-nav-toggle').removeClass('active');
-				
-	    	}
-		});
-	};
-
-
+    
+	// Burger Menu
 	var burgerMenu = function() {
-
-		$('body').on('click', '.js-gtco-nav-toggle', function(event){
-			var $this = $(this);
-
-
-			if ( $('body').hasClass('overflow offcanvas') ) {
-				$('body').removeClass('overflow offcanvas');
+		$('body').on('click', '.js-fh5co-nav-toggle', function(event){
+			if ( $('#navbar').is(':visible') ) {
+				$(this).removeClass('active');	
 			} else {
-				$('body').addClass('overflow offcanvas');
+				$(this).addClass('active');	
 			}
-			$this.toggleClass('active');
 			event.preventDefault();
+		});
+	};
+    // Page Nav
+	var clickMenu = function() {
 
+		$('a:not([class="external"])').click(function(event){
+			var section = $(this).data('nav-section'),
+				navbar = $('#navbar');
+		    $('html, body').animate({
+		        scrollTop: $('[data-section="' + section + '"]').offset().top
+		    }, 500);
+
+		    if ( navbar.is(':visible')) {
+		    	navbar.removeClass('in');
+		    	navbar.attr('aria-expanded', 'false');
+		    	$('.js-fh5co-nav-toggle').removeClass('active');
+		    }
+
+		    event.preventDefault();
+		    return false;
+		});
+	};
+    // Reflect scrolling in navigation
+	var navActive = function(section) {
+
+		var $el = $('#navbar > ul');
+		$el.find('li').removeClass('active');
+		$el.each(function(){
+			$(this).find('a[data-nav-section="'+section+'"]').closest('li').addClass('active');
+		});
+
+	};
+	var navigationSection = function() {
+
+		var $section = $('div[data-section]');
+		
+		$section.waypoint(function(direction) {
+		  	if (direction === 'down') {
+		    	navActive($(this.element).data('section'));
+		    
+		  	}
+		}, {
+		  	offset: '150px'
+		});
+
+		$section.waypoint(function(direction) {
+		  	if (direction === 'up') {
+		    	navActive($(this.element).data('section'));
+		  	}
+		}, {
+		  	offset: function() { return -$(this.element).height() + 155; }
+		});
+
+	};
+	// Window Scroll
+	var windowScroll = function() {
+		var lastScrollTop = 0;
+
+		$(window).scroll(function(event){
+
+		   var header = $('#fh5co-header'),
+				scrlTop = $(this).scrollTop();
+
+			if ( scrlTop > 500 && scrlTop <= 2000 ) {
+				header.addClass('navbar-fixed-top fh5co-animated slideInDown');
+			} else if ( scrlTop <= 500) {
+				if ( header.hasClass('navbar-fixed-top') ) {
+					header.addClass('navbar-fixed-top fh5co-animated slideOutUp');
+					setTimeout(function(){
+						header.removeClass('navbar-fixed-top fh5co-animated slideInDown slideOutUp');
+					}, 100 );
+				}
+			} 
+			
 		});
 	};
 
@@ -344,22 +349,42 @@
 		}
 	};
     
+    // Window Scroll
+	var windowScroll = function() {
+		var lastScrollTop = 0;
+
+		$(window).scroll(function(event){
+
+		   var scrlTop = $(this).scrollTop();
+
+			if ( scrlTop > 500 && scrlTop <= 2000 ) {
+				//header.addClass('navbar-fixed-top fh5co-animated slideInDown');
+			} else if ( scrlTop <= 500) {
+				//if ( header.hasClass('navbar-fixed-top') ) {
+				//	header.addClass('navbar-fixed-top fh5co-animated slideOutUp');
+				//	setTimeout(function(){
+				//		header.removeClass('navbar-fixed-top fh5co-animated slideInDown slideOutUp');
+				//	}, 100 );
+				//}
+			}
+			
+		});
+	};
+    
     var getOnGooglePlay = function() {
         $(".get_on_android").on("click", function(){
            location.href="https://play.google.com/store/apps/details?id=com.coinbank.worldspon";
         });
     }
     
-    var preventClickEvent = function() {
-        $(document).on('click', 'a[href="#"]', function(e){
-            e.preventDefault();
+    var moveToAbout = function() {
+        $(document).on('click', function(){
+            $(document).focus('#aboutWorldSpon').addClass('animated')
         });
     }
 
 	
 	$(function(){
-		mobileMenuOutsideClick();
-		offcanvasMenu();
 		burgerMenu();
 		contentWayPoint();
 		dropdown();
@@ -370,7 +395,9 @@
 		counterWayPoint();
 		changeWayPoint();
         getOnGooglePlay();
-        preventClickEvent();
+        moveToAbout();
+        windowScroll();
+        clickMenu();
 	});
 
 
